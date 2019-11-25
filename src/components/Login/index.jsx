b1aa03ed25317a7921ca09608b2a01b797c27223
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-11-25 18:59:08 +0300

    feat: add tokens login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,27 +2,33 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/react-hooks";
 import { LOGIN } from "../../mutation/LOGIN";
 import { useCookies } from "react-cookie";
+import { Redirect } from "react-router-dom";
+import Routers from "../../Router";
 
 export const Login = () => {
   let [email, setEmail] = useState("");
   let [hashPassword, setHashPassword] = useState("");
+  let [isLoged, setLogin] = useState("");
   let [cookies, setCookies] = useCookies(["access-token"]);
   const [login, { error }] = useMutation(LOGIN, {
     variables: { email, hashPassword }
   });
 
   const handleSubmit = async () => {
-    const data = await login();
-    setCookies("access-token", data.data.login.accessToken);
-    setCookies("refresh-token", data.data.login.refreshToken);
-    console.log(data);
+    try {
+      const { data } = await login();
+      setCookies("access-token", data.login.accessToken);
+      setCookies("refresh-token", data.login.refreshToken);
+      setLogin(true);
+    } catch (error) {
+      console.log(error);
+      setLogin(false);
+    }
   };
 
-  if (error) {
-    console.log("error: ", error);
-  }
   return (
     <>
+      {isLoged && <Redirect to="/Home" />}
       <input type="text" onChange={e => setEmail(e.target.value)} />
       <input type="text" onChange={e => setHashPassword(e.target.value)} />
       <button onClick={handleSubmit}>Login</button>
