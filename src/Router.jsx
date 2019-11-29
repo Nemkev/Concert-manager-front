49bd16a4acfe77d9d0f115d24c3ba4dc6c341479
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-11-29 17:34:06 +0300

    feat: add mansontry to user

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import { Example } from "./components/example";
 import { Login } from "./components/Login";
 import { Registration } from "./components/Registration";
 import { User } from "./components/User";
+import { Concerts } from "./components/Concerts";
 import {
   BrowserRouter as Router,
   Switch,
@@ -20,6 +21,7 @@ export const Routers = () => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/registration" component={Registration} />
         <Route exact path="/user" component={User} />
+        <Route exact path="/concerts" component={Concerts} />
         <Redirect from="/" to="home" />
       </Switch>
     </Router>
