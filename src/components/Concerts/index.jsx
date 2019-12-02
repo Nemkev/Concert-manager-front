cb561e65e5fa9628cffce55c41689fd5c3afaabb
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-12-02 12:04:38 +0300

    refactor: add changes after review

--- src/components/Concerts/index.jsx
+++ src/components/Concerts/index.jsx
@@ -5,99 +5,97 @@ import "./index.scss";
 
 export const Concerts = () => {
   return (
-    <>
-      <div className="overlap">
-        <div className="filter-zone">
-          <input type="text" className="filter-input"></input>
-        </div>
-
-        <XMasonry maxColumns={5}>
-          <XBlock>
-            <div className="card">
-              <h1>Simple Card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock width={2}>
-            <div className="card">
-              <h1>Wider card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock>
-            <div className="card">
-              <h1>Simple Card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock width={3}>
-            <div className="card">
-              <h1>Wider card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock>
-            <div className="card">
-              <h1>Simple Card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock width={2}>
-            <div className="card">
-              <h1>Wider card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock>
-            <div className="card">
-              <h1>Simple Card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock width={2}>
-            <div className="card">
-              <h1>Wider card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock>
-            <div className="card">
-              <h1>Simple Card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock width={5}>
-            <div className="card">
-              <h1>Wider card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock>
-            <div className="card">
-              <h1>Simple Card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock width={2}>
-            <div className="card">
-              <h1>Wider card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock>
-            <div className="card">
-              <h1>Simple Card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-          <XBlock width={2}>
-            <div className="card">
-              <h1>Wider card</h1>
-              <p>Any text!</p>
-            </div>
-          </XBlock>
-        </XMasonry>
+    <div className="overlap">
+      <div className="filter-zone">
+        <input type="text" className="filter-input"></input>
       </div>
-    </>
+
+      <XMasonry maxColumns={5}>
+        <XBlock>
+          <div className="card">
+            <h1>Simple Card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock width={2}>
+          <div className="card">
+            <h1>Wider card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock>
+          <div className="card">
+            <h1>Simple Card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock width={3}>
+          <div className="card">
+            <h1>Wider card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock>
+          <div className="card">
+            <h1>Simple Card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock width={2}>
+          <div className="card">
+            <h1>Wider card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock>
+          <div className="card">
+            <h1>Simple Card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock width={2}>
+          <div className="card">
+            <h1>Wider card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock>
+          <div className="card">
+            <h1>Simple Card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock width={5}>
+          <div className="card">
+            <h1>Wider card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock>
+          <div className="card">
+            <h1>Simple Card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock width={2}>
+          <div className="card">
+            <h1>Wider card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock>
+          <div className="card">
+            <h1>Simple Card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+        <XBlock width={2}>
+          <div className="card">
+            <h1>Wider card</h1>
+            <p>Any text!</p>
+          </div>
+        </XBlock>
+      </XMasonry>
+    </div>
   );
 };
