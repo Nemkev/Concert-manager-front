1c4e0a18bc91fa7254b03c4bc0e3fb1a7370ec04
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-12-03 14:38:38 +0300

    feat: add timeout for request for searching concerts

--- src/components/Concerts/index.jsx
+++ src/components/Concerts/index.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { XMasonry, XBlock } from "react-xmasonry";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_CONCERTS } from "../../query/GET_CONCERTS";
+import { useDebouncedCallback } from "use-debounce";
 
 import "./index.scss";
 
 export const Concerts = () => {
+  const [concerts, setConcerts] = useState("");
+  const [debouncedCallback] = useDebouncedCallback(concerts => {
+    setConcerts(concerts);
+  }, 1000);
   const { loading, error, data } = useQuery(GET_CONCERTS, {
-    variables: { name: "" }
+    variables: { name: concerts }
   });
+
   if (loading) return <p>Loading ...</p>;
-  console.log(data, 11);
+  console.log(data.getConcerts, 11);
 
   return (
     <div className="overlap">
       <div className="filter-zone">
-        <input type="text" className="filter-input"></input>
+        <input
+          type="text"
+          name="concerts"
+          onChange={e => debouncedCallback(e.target.value)}
+          className="filter-input"
+        ></input>
       </div>
 
-      <XMasonry maxColumns={5}>
+      <XMasonry maxColumns={4}>
         <XBlock>
           <div className="card">
             <h2>Simple Card</h2>
