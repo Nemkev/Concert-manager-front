a858d1539f65edab89481608e281ea628c1ae2ce
Zhenya Nemkevich <ostegoelectric@gmail.com>
2019-12-17 16:33:28 +0300

    feat: add responsive

--- src/components/Concerts/index.jsx
+++ src/components/Concerts/index.jsx
@@ -53,38 +53,39 @@ export const Concerts = () => {
           className="filter-input"
         ></input>
       </div>
-
-      {loading || loadingAdditionalFilters ? (
-        <p>Loading ...</p>
-      ) : (
-        <select name="city" value="city" onChange={handleChangeCity}>
-          {additionalFiltersData.getFilter.map(item => (
-            <option value={item.city}>{item.city}</option>
-          ))}
-        </select>
-      )}
-      {loading || loadingAdditionalFilters ? (
-        <p>Loading ...</p>
-      ) : (
-        <select name="date" value="date" onChange={handleChangeDate}>
-          {[
-            ...new Set(
+      <div className="select-bar">
+        {loading || loadingAdditionalFilters ? (
+          <p>Loading ...</p>
+        ) : (
+          <select name="city" value="city" onChange={handleChangeCity}>
+            {additionalFiltersData.getFilter.map(item => (
+              <option value={item.city}>{item.city}</option>
+            ))}
+          </select>
+        )}
+        {loading || loadingAdditionalFilters ? (
+          <p>Loading ...</p>
+        ) : (
+          <select name="date" value="date" onChange={handleChangeDate}>
+            {[
               ...new Set(
-                additionalFiltersData.getFilter.map(item => {
-                  return item.concerts.map(secondItem => (
-                    <option value={secondItem.date}>{secondItem.date}</option>
-                  ));
-                })
+                ...new Set(
+                  additionalFiltersData.getFilter.map(item => {
+                    return item.concerts.map(secondItem => (
+                      <option value={secondItem.date}>{secondItem.date}</option>
+                    ));
+                  })
+                )
               )
-            )
-          ]}
-        </select>
-      )}
+            ]}
+          </select>
+        )}
+      </div>
 
       {loading || loadingAdditionalFilters ? (
         <p>Loading ...</p>
       ) : (
-        <XMasonry maxColumns={3}>
+        <XMasonry maxColumns={3} className="masonry">
           {data.getFilter.map(item => (
             <XBlock width={widthRandomize(3)} key={item.id}>
               <div className="card">
