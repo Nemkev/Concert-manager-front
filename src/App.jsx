5cc4ac9b8e45d88361c3b160aed9036c343c9558
Zhenya Nemkevich <ostegoelectric@gmail.com>
2020-01-13 11:09:22 +0300

    refactor: change routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Header } from "./modules/Header";
 import { Footer } from "./modules/Footer";
 import { Routers } from "./Router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "reset-css";
 import "./App.css";
 
 export const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <Routers />
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- src/Router.jsx
+++ src/Router.jsx
@@ -6,30 +6,23 @@ import { User } from "./components/User";
 import { Concerts } from "./components/Concerts";
 import { Booking } from "./components/Booking";
 import { About } from "./components/About";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import "./App.css";
 
 export const Routers = () => {
   return (
     <div className="wrapper">
-      <Router>
-        <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/registration" component={Registration} />
-          <Route exact path="/user" component={User} />
-          <Route exact path="/concerts" component={Concerts} />
-          <Route exact path="/booking" component={Booking} />
-          <Route exact path="/about" component={About} />
-          <Redirect from="/" to="home" />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/registration" component={Registration} />
+        <Route exact path="/user" component={User} />
+        <Route exact path="/concerts" component={Concerts} />
+        <Route exact path="/booking" component={Booking} />
+        <Route exact path="/about" component={About} />
+        <Redirect from="/" to="home" />
+      </Switch>
     </div>
   );
 };
